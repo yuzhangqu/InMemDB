446623e888aaf04d0c9fe72a7d869c9f4e5253da
yuzhangqu <yuzhangqu@qq.com>
2014-04-29 00:14:35 +0800

    init commit server header file

---
+++ server.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <queue>
+#include <unordered_map>
+
+#define NUM_WORKERS 8
+#define NUM_HANDLES 10
+
+enum xxop {
+    GET,
+    PUT,
+    GETRANGE
+};
